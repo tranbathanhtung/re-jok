b75da473ec5e185e689b513d5bc743baebc693ee
tranbathanhtung <tranbathanhtung222@gmail.com>
2018-05-28 22:34:54 +0700

    Add demo badge component

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import {ThemeProvider} from 'styled-components';
 import {defaultTheme} from './theme';
 import {ButtonDemo} from './Button/demo'
 import {AvatarDemo} from './Avatar/demo'
+import {BadgeDemo} from './Badge/demo'
 
 
 
 ReactDOM.render(
-  
+
 
      <ThemeProvider theme={defaultTheme}>
        <React.Fragment>
 
        <ButtonDemo/>
-
-
        <AvatarDemo/>
+       <BadgeDemo/>
        </React.Fragment>
      </ThemeProvider>
 

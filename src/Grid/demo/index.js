6725825d94ceeff4b44b1a59d4ae63febbcba179
tranbathanhtung <tranbathanhtung222@gmail.com>
2018-06-06 22:29:41 +0700

    add demo Grid

--- src/Grid/demo/index.js
+++ src/Grid/demo/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
+// @flow
+import  * as React from 'react';
 import Row from '../Row';
 import Col from '../Col';
-import '../../index.css'
+import '../../index.css';
+import styled from 'styled-components';
 
-class GridDemo extends React.Component {
+const DemoBox = styled.div `
+  height: ${props => `${props.height}px`};
+  line-height: ${props => `${props.height}px`};
+`
+
+class GridDemo extends React.Component<{}> {
   render() {
     return (<React.Fragment>
       {/* Basic */}
@@ -46,6 +53,22 @@ class GridDemo extends React.Component {
         </Col>
       </Row>
 
+      <Row gutter={{xs: 8, sm: 16, lg: 40}}>
+        <Col xs={2} sm={4} md={6} lg={10} className="col">
+          <div className="gutter">Gutter with Respon</div>
+        </Col>
+        <Col  xs={20} sm={16} md={12} lg={4} className="col">
+          <div className="gutter">Gutter with Respon</div>
+
+        </Col>
+        <Col xs={2} sm={4} md={6} lg={10} className="col">
+          <div className="gutter">Gutter with Respon</div>
+
+        </Col>
+
+
+      </Row>
+
       {/* Offset */}
       <Row>
         <Col span={8} className="col-demo col">Col-8 Offset-8</Col>
@@ -65,10 +88,113 @@ class GridDemo extends React.Component {
 
       </Row>
 
+      {/* Push, Pull */}
+
       <Row>
         <Col span={18} push={6} className="col-demo col">Col-18 Col-push-6</Col>
         <Col span={6} pull={18} className="col-demo col">Col-6 Col-pull-18</Col>
       </Row>
+
+      {/* Justify */}
+      <Row justify="flex-start">
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+      </Row>
+
+      <Row justify="center">
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+      </Row>
+
+      <Row justify="flex-end">
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+      </Row>
+
+      <Row justify="space-between">
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+      </Row>
+
+      <Row justify="space-around">
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+        <Col className="col-demo col" span={4}>Col-4</Col>
+      </Row>
+
+      {/* Align */}
+      <Row justify="center" align="flex-start">
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={100}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={50}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={120}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={80}>Col-4</DemoBox>
+        </Col>
+      </Row>
+
+      <Row justify="space-around" align="center">
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={100}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={50}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={120}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={80}>Col-4</DemoBox>
+        </Col>
+      </Row>
+
+      <Row justify="space-between" align="flex-end">
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={100}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={50}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={120}>Col-4</DemoBox>
+        </Col>
+        <Col className="col-demo col" span={4}>
+          <DemoBox height={80}>Col-4</DemoBox>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="col-demo col" span={6} order={3}>1 Col-order-3</Col>
+        <Col className="col-demo col" span={6} order={2}>2 Col-order-2</Col>
+        <Col className="col-demo col" span={6} order={4}>3 Col-order-4</Col>
+        <Col className="col-demo col" span={6} order={1}>4 Col-order-1</Col>
+      </Row>
+
+      <Row>
+        <Col className="col-demo col" xs={2} sm={4} md={6} lg={10}>Col</Col>
+        <Col className="col-demo col" xs={20} sm={16} md={12} lg={4}>Col</Col>
+        <Col className="col-demo col" xs={2} sm={4} md={6} lg={10}>Col</Col>
+      </Row>
+
+      <Row>
+    <Col className="col-demo col" xs={{ span: 5, offset: 1 }} md={{ span: 6, offset: 2 }}>Col</Col>
+    <Col className="col-demo col" xs={{ span: 11, offset: 1 }} md={{ span: 6, offset: 2 }}>Col</Col>
+    <Col className="col-demo col" xs={{ span: 5, offset: 1 }} md={{ span: 6, offset: 2 }}>Col</Col>
+  </Row>
 
     </React.Fragment>)
   }

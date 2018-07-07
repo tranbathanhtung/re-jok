732222fe545d5e5e49970514eea2e6b031d3dd8f
tranbathanhtung <tranbathanhtung222@gmail.com>
2018-07-07 23:01:25 +0700

    create typography component

---
+++ src/Typography/demo/index.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react';
+
+import {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  P,
+  Span
+} from '../index';
+
+class TypographyDemo extends React.Component<{}>{
+  render() {
+    return (<React.Fragment>
+      <H1>Heading Primary</H1>
+      <H2>Heading Primary</H2>
+
+      <H3>Heading Primary</H3>
+      <H4>Heading Primary</H4>
+      <H5>Heading Primary</H5>
+      <H6>Heading Primary</H6>
+      <P>Heading Primary</P>
+      <Span>Heading Primary</Span>
+    
+
+    </React.Fragment>)
+  }
+}
+
+
+export default TypographyDemo

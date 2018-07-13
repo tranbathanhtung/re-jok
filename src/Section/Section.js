4d4d8de09c062b51d98ef740294a12c2a53daad7
tranbathanhtung <tranbathanhtung222@gmail.com>
2018-07-13 22:45:00 +0700

    create component section

---
+++ src/Section/Section.js
@@ -0,0 +1,49 @@
+//@flow
+import * as React from 'react';
+import {
+  StyledSection
+} from './style';
+
+import { isChild } from '../helpers/typeUtils';
+
+
+
+type Props = {
+  /** Add style to Section**/
+  style?: Object,
+  /** Add class to Section**/
+  className?: string,
+  /** Children of Section could be anything**/
+  children: any,
+  /** Type of Section**/
+  type: 'default' | 'background',
+  /** backgroundUrl work only when tyoe of section is background**/
+  backgroundUrl?: string
+}
+
+const defaultProps = {
+  type: 'default'
+}
+
+class Section extends React.Component<Props>{
+
+  static defaultProps = defaultProps;
+
+  render(){
+    const {
+      children,
+      ...rest
+    } = this.props;
+
+    const hasChild = !isChild(children);
+
+    return (
+      <StyledSection {...rest}>
+
+        {hasChild && children}
+      </StyledSection>
+    )
+  }
+}
+
+export default Section
---
+++ src/Section/style.js
@@ -0,0 +1,45 @@
+import styled,{css} from 'styled-components';
+
+
+
+export const StyledSection = styled.div`
+  font-size: 1.4rem;
+  background-color: ${({theme}) => theme.background.default};
+  ${
+    props => props.type === "default"
+    ? css`
+       color: ${({theme}) => theme.text.regular};
+
+       background-color: ${({theme}) => theme.background.default};
+
+    `: props.type === "background" && props.backgroundUrl
+    ? css`
+      background-image: url(${props.backgroundUrl});
+      background-size: cover;
+      color: ${({theme}) => theme.text.default};
+
+      background-position: 50% 50%;
+      background-repeat: no-repeat;
+      height: auto;
+    `
+    : null
+  }
+  position: relative;
+  box-sizing: border-box;
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+
+  &:after, &:before {
+    content: "";
+    display: table;
+  }
+
+  &:after{
+    clear: both;
+  }
+
+  @media (max-width: 960px) {
+     padding-top: 7rem;
+     padding-bottom: 7rem;
+  }
+`
